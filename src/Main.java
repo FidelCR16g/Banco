e46bf4808c30de8f9d1a33d5399350bec8a84def
fave6 <fidelcr16g@gmail.com>
2025-05-23 21:01:38 -0600

    Comit

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        //ccax
-        //hola
-        //Top 10 empanadas de peru
-        System.out.println("Hola fide y xca, soy ana");
-        //prueba nueva
-        System.out.println("Otra pueba.");
+        Sesion sesion = new Sesion("FidelCR16", "EloteAsao98", 1234);
+        Sesion sesion1 = new Sesion("AnaG53", "EloteAsao25", 1423);
+        Sesion sesion2 = new Sesion("XcaretGC98", "EloteAsao39", 4321);
+
+        sesion2.iniciarSesion();
     }
 }
--- src/Sesion.java
+++ src/Sesion.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Sesion {
     private String usuario;
     private String contrasenia;
@@ -35,6 +37,32 @@ public class Sesion {
     }
 
     public void iniciarSesion(){
+        Scanner entrada = new Scanner(System.in);
+        String usuario;
+        String contrasenia;
+        int nip;
 
+        while (true) {
+            System.out.println("INICIAR SESION.");
+
+            System.out.print("Usuario: ");
+            usuario = entrada.nextLine();
+
+            System.out.print("Contraseña: ");
+            contrasenia = entrada.nextLine();
+
+            System.out.print("NIP: ");
+            nip = entrada.nextInt();
+            entrada.nextLine();
+
+            if (!usuario.equals(getUsuario()) ||
+                    !contrasenia.equals(getContrasenia()) ||
+                    nip != getNip()) {
+                System.out.println("Por favor ingresa los datos correctos.\n");
+            } else {
+                System.out.println("Bienvenido " + usuario + "!\n");
+                break;
+            }
+        }
     }
 }
