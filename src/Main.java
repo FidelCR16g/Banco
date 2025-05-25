242b3fcfc3f80a6a6437ac0020c54c80e094fb78
XcaretGar <xcaret.gc4@gmail.com>
2025-05-24 20:46:34 -0600

    contraseñas

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        Sesion sesion = new Sesion("FidelCR16", "EloteAsao98", 1234);
-        Sesion sesion1 = new Sesion("AnaG53", "EloteAsao25", 1423);
-        Sesion sesion2 = new Sesion("XcaretGC98", "EloteAsao39", 4321);
+        Sesion sesion = new Sesion("FidelCR16", "fide", 1234);
+        Sesion sesion1 = new Sesion("AnaRO53", "ana", 1423);
+        Sesion sesion2 = new Sesion("XcaretGC19", "xca", 4321);
 
         sesion2.iniciarSesion();
     }
