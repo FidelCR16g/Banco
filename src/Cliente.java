6d7d605f73223367d7d938b958777ef8786c9ccd
XcaretGar <xcaret.gc4@gmail.com>
2025-05-24 22:55:28 -0600

    cuenta y cliente

--- src/Cliente.java
+++ src/Cliente.java
@@ -1,2 +1,54 @@
 public class Cliente {
+    private String nombreC;
+    private String ine;
+    private String fechaNacimiento;
+    private String lugarNacimiento;
+    private String telefonoC;
+    private String correoC;
+    private Cuenta cuenta;
+
+    public Cliente() {}
+
+    public Cliente(String nombreC, String ine, String fechaNacimiento, String lugarNacimiento, String telefonoC, String correoC, Cuenta cuenta) {
+        this.nombreC = nombreC;
+        this.ine = ine;
+        this.fechaNacimiento = fechaNacimiento;
+        this.lugarNacimiento = lugarNacimiento;
+        this.telefonoC = telefonoC;
+        this.correoC = correoC;
+        this.cuenta = cuenta;
+    }
+
+    public String getNombreC() {
+        return nombreC;
+    }
+
+    public String getIne() {
+        return ine;
+    }
+
+    public String getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public String getLugarNacimiento() {
+        return lugarNacimiento;
+    }
+
+    public String getTelefonoC() {
+        return telefonoC;
+    }
+
+    public String getCorreoC() {
+        return correoC;
+    }
+
+    public void mostrarDatosCliente() {
+        System.out.println("CLIENTE");
+        System.out.println("Nombre: " + getNombreC() + "\nINE: " + getIne() + "\nFecha de Nacimiento: " + getFechaNacimiento() + "\nLugar de Nacimiento: " + getLugarNacimiento() + "\nTeléfono: " +  getTelefonoC() + "\nCorreo: " + getCorreoC());
+    }
+
+    public void asignarCuenta(Cuenta cuenta) {
+
+    }
 }
--- src/Cuenta.java
+++ src/Cuenta.java
@@ -1,4 +1,32 @@
-public class Cuenta {
+public abstract class Cuenta {
+    public enum TipoCuenta {
+        INVERSION,
+        NOMINA,
+        CORRIENTE,
+        CREDITO
+    }
+
+    protected String numeroCuenta;
+    protected TipoCuenta tipoCuenta;
+    protected double monto;
 
     public Cuenta(){}
+
+    public Cuenta(String numeroCuenta, TipoCuenta tipoCuenta, double monto) {
+        this.numeroCuenta = numeroCuenta;
+        this.tipoCuenta = tipoCuenta;
+        this.monto = monto;
+    }
+
+    public abstract void mostrarCuenta();
+
+    public abstract void consultarSaldo();
+
+    public double retirar(double monto) {
+
+    }
+
+    public double depositar(double monto) {
+
+    }
 }
