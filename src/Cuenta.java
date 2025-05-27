8078a67b672a2796ecded526ca46f7d04cba0c90
XcaretGar <xcaret.gc4@gmail.com>
2025-05-27 11:09:47 -0600

    ticket

--- src/Cuenta.java
+++ src/Cuenta.java
@@ -4,7 +4,6 @@ public abstract class Cuenta {
     public enum TipoCuenta {
         INVERSION,
         NOMINA,
-        CORRIENTE,
         CREDITO
     }
 
---
+++ src/Ticket.java
@@ -0,0 +1,17 @@
+public class Ticket {
+    private String fechaGeneracionTicket;
+
+    public Ticket(){}
+
+    public Ticket(String fechaGeneracionTicket) {
+        this.fechaGeneracionTicket = fechaGeneracionTicket;
+    }
+
+    public void generarTxt() {
+
+    }
+
+    public void guardarTxt() {
+
+    }
+}
