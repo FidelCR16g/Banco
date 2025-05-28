2209b4cfa2e8c50fa4bd70c71c0af50c52a64e01
XcaretGar <xcaret.gc4@gmail.com>
2025-05-28 11:11:34 -0600

    cuentas

--- src/Ticket.java
+++ src/Ticket.java
@@ -1,3 +1,6 @@
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Ticket {
     private String fechaGeneracionTicket;
 
@@ -11,7 +14,12 @@ public class Ticket {
 
     }
 
-    public void guardarTxt() {
-
+    public void guardarTxt(String contenido, String rutaArchivo) {
+        try (FileWriter writer = new FileWriter(rutaArchivo)) {
+            writer.write(contenido);
+            System.out.println("Ticket guardado en " + rutaArchivo);
+        } catch (IOException e) {
+            System.out.println("Error al guadar: " + e.getMessage());
+        }
     }
 }
