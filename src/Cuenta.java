3ffec182e950215daf9eb4da9b310254916400dc
Ana Georgina <anarosorio@hotmail.com>
2025-05-28 10:13:03 -0600

    hola

--- src/Cuenta.java
+++ src/Cuenta.java
@@ -4,22 +4,27 @@ public abstract class Cuenta {
     public enum TipoCuenta {
         INVERSION,
         NOMINA,
-        CORRIENTE,
         CREDITO
     }
 
     protected String numeroCuenta;
     protected TipoCuenta tipoCuenta;
     protected double saldo;
+    protected int nip;
     private Cliente cliente;
 
     public Cuenta(){}
 
-    public Cuenta(String numeroCuenta, TipoCuenta tipoCuenta, double saldo, Cliente cliente) {
+    public Cuenta(String numeroCuenta, TipoCuenta tipoCuenta, double saldo, int nip, Cliente cliente) {
         this.numeroCuenta = numeroCuenta;
         this.tipoCuenta = tipoCuenta;
         this.saldo = saldo;
+        this.nip = nip;
         this.cliente = cliente;
+    }
+
+    protected int getNip(){
+        return nip;
     }
 
     public void setCliente(Cliente cliente) {
@@ -31,42 +36,75 @@ public abstract class Cuenta {
     }
 
     public void retirar() {
-        Scanner scanner = new Scanner(System.in);
 
-        while (true) {
-            System.out.println("Ingresa el monto a retirar: ");
-            double monto = scanner.nextDouble();
+        if (nipValido()){
+            Scanner scanner = new Scanner(System.in);
 
-            if (monto < 0) {
-                System.out.println("El monto a retirar debe ser una cantidad positiva");
-            } else if (monto > 9000) {
-                System.out.println("No puedes retirar una cantidad mayor a 9000");
-            } else if (saldo < monto) {
-                System.out.println("Saldo insuficiente. Tu saldo es: " + saldo);
-            } else {
-                saldo -= monto;
-                break;
+            while (true) {
+                System.out.println("Ingresa el monto a retirar: ");
+                double monto = scanner.nextDouble();
+
+                if (monto < 0) {
+                    System.out.println("El monto a retirar debe ser una cantidad positiva");
+                } else if (monto > 9000) {
+                    System.out.println("No puedes retirar una cantidad mayor a 9000");
+                } else if (saldo < monto) {
+                    System.out.println("Saldo insuficiente. Tu saldo es: " + saldo);
+                } else {
+                    saldo -= monto;
+                    break;
+                }
             }
+        }else{
+            System.out.println(" ");
         }
     }
 
     public void depositar() {
-        Scanner scanner = new Scanner(System.in);
+        if (nipValido()){
+            Scanner scanner = new Scanner(System.in);
 
-        while (true) {
-            System.out.println("Ingresa el monto a depositar: ");
-            double monto = scanner.nextDouble();
+            while (true) {
+                System.out.println("Ingresa el monto a depositar: ");
+                double monto = scanner.nextDouble();
 
-            if (monto < 0) {
-                System.out.println("El monto a depositar debe ser una cantidad positiva");
-            } else if (saldo < monto) {
-                System.out.println("Saldo insuficiente. Tu saldo es: " + saldo);
-            } else {
-                saldo += monto;
-                break;
+                if (monto < 0) {
+                    System.out.println("El monto a depositar debe ser una cantidad positiva");
+                } else if (saldo < monto) {
+                    System.out.println("Saldo insuficiente. Tu saldo es: " + saldo);
+                } else {
+                    saldo += monto;
+                    break;
+                }
             }
+        }else{
+            System.out.println(" ");
         }
     }
+
+    protected boolean nipValido() {
+        Scanner entrada = new Scanner(System.in);
+        int numeroIntentos = 4;
+
+        while (numeroIntentos > 0) {
+            System.out.print("Por favor ingrese su NIP: ");
+            int nipIngresado = entrada.nextInt();
+
+            if (nipIngresado == (getNip())) {
+                return true;
+            } else {
+                numeroIntentos--;
+                if (numeroIntentos > 0) {
+                    System.out.println("NIP incorrecto, te quedan " + numeroIntentos + " intentos.\n");
+                } else {
+                    System.out.println("Te quedaste sin intentos.");
+                }
+            }
+        }
+
+        return false;
+    }
+
 
     public void consultarSaldo() {
         System.out.println("Tu saldo actual es: " + saldo);
