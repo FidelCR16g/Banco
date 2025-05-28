dd51b39c71acb412df6e298aa940ec4ed5d2b4fc
fave6 <fidelcr16g@gmail.com>
2025-05-28 11:12:32 -0600

    Comit

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,51 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        List<Cliente> clientes = new ArrayList<>();
 
-        Cliente cliente = new Cliente("Ana Georgina", "COEUDJ56464", "9381194597", "anarosorio@hotmail.com", "ANA15", "1234");
-        Inversion inversion = new Inversion("INV001", 10000.0, 1253, 2.0, 12, cliente);
+        Cliente cliente1 = new Cliente("Ana Pérez", "INE123", "1234567890", "ana@mail.com", "ana123", "clave1");
+        Cliente cliente2 = new Cliente("Luis Gómez", "INE456", "0987654321", "luis@mail.com", "luis456", "clave2");
 
-        cliente.asignarCuenta(inversion);
-        inversion.mostrarCuenta();
+        clientes.add(cliente1);
+        clientes.add(cliente2);
+
+        int numeroIntentos = 4;
+        boolean sesionExitosa = false;
+        Cliente clienteActual = null;
+
+        while (numeroIntentos > 0 && !sesionExitosa) {
+            System.out.print("Usuario: ");
+            String usuarioIngresado = entrada.nextLine();
+
+            System.out.print("Contraseña: ");
+            String contraseniaIngresada = entrada.nextLine();
+
+            for (Cliente cliente : clientes) {
+                if (cliente.sesionValida(usuarioIngresado, contraseniaIngresada)) {
+                    System.out.println("Bienvenido " + cliente.getNombreC() + "!");
+                    sesionExitosa = true;
+                    clienteActual = cliente;
+                    break;
+                }
+            }
+
+            if (!sesionExitosa) {
+                numeroIntentos--;
+                System.out.println("Datos incorrectos. Te quedan " + numeroIntentos + " intentos.\n");
+            }
+        }
+
+        if (!sesionExitosa) {
+            System.out.println("Has alcanzado el número máximo de intentos.");
+        } else {
+            clienteActual.mostrarDatosCliente();
+        }
+
+        entrada.close();
 
     }
 }
